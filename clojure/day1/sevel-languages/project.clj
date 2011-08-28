3f62589c452de35a084f803c4e6f0fdb9163f454
John Hilts <johnhilts@gmail.com>
2011-08-27 20:05:27 -0700

    Clojure Day 1'

---
+++ clojure/day1/sevel-languages/project.clj
@@ -0,0 +1,3 @@
+(defproject sevel-languages "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ clojure/day1/sevel-languages/test/sevel_languages/test/core.clj
@@ -0,0 +1,6 @@
+(ns sevel-languages.test.core
+  (:use [sevel-languages.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
